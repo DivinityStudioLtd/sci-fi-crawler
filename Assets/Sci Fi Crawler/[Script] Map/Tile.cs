90f233aab9ef94c53992b623d48a09c3e69e0dcd
Magarthryx <magarthryx@gmail.com>
2013-07-14 19:44:14 -0400

    clear because incorrect tortoise export

--- Assets/Sci Fi Crawler/[Script] Map/Tile.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using System.Collections;
-
-public class Tile : Entity {
-	public int x;
-	public int y;
-
-}
--- Assets/[Linker]/Interface/InterfaceLogin.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using System.Collections;
-
-public class InterfaceLogin : Interface {
-	public override bool SetDisplay (bool newDisplay) {
-		return base.SetDisplay (newDisplay);
-	}
-}
